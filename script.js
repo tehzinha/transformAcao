6e9ef5461071d6bdee4f89c2a7c85a15f09ddd46
tehzinha <estervrsampaio47@gmail.com>
2025-06-13 15:01:52 -0300

    cadastrando envio de email no forms

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function verOrganograma() {
   window.open('img/organograma.png', '_blank', 'width=800,height=600');
 }
+document.getElementById("voluntario-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // impede envio padrão do formulário
+
+  emailjs.sendForm("service_f3w95js", "template_xv8exuz", this)
+    .then(function() {
+      alert("Mensagem enviada com sucesso! Obrigado pelo contato.");
+      document.getElementById("voluntario-form").reset();
+    }, function(error) {
+      alert("Ocorreu um erro: " + JSON.stringify(error));
+    });
+});
