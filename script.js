b62f50c84e468fc427b0fe6bd25d301c887a7163
tehzinha <estervrsampaio47@gmail.com>
2025-06-16 20:20:19 -0300

    erro na lupa

--- script.js
+++ script.js
@@ -9,28 +9,26 @@ function fecharOrganograma() {
 document.addEventListener("DOMContentLoaded", () => {
   const img = document.getElementById("organogramaImg");
   const lupa = document.getElementById("lupa");
-  const lupaImg = document.getElementById("lupaImg");
 
   img.addEventListener("mousemove", function(e) {
-    lupa.style.display = "block";
-
     const rect = img.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    // posição da lupa
+    lupa.style.display = "block";
     lupa.style.left = (e.pageX - 75) + "px";
     lupa.style.top = (e.pageY - 75) + "px";
 
-    // mover imagem dentro da lupa
-    lupaImg.style.left = (-x * 2 + 75) + "px";
-    lupaImg.style.top = (-y * 2 + 75) + "px";
+    lupa.style.backgroundImage = `url(${img.src})`;
+    lupa.style.backgroundSize = `${img.width * 2}px ${img.height * 2}px`; // zoom 2x
+    lupa.style.backgroundPosition = `-${x * 2 - 75}px -${y * 2 - 75}px`;
   });
 
   img.addEventListener("mouseleave", function() {
     lupa.style.display = "none";
   });
 });
+
 
 document.getElementById("voluntario-form").addEventListener("submit", function(event) {
   event.preventDefault(); // impede envio padrão do formulário
