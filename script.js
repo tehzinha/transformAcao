fa484c905bdf92be0cef95245f85709a0ddd1a31
tehzinha <estervrsampaio47@gmail.com>
2025-06-16 20:46:42 -0300

    desandou

--- script.js
+++ script.js
@@ -41,19 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Formulário voluntário (não alterado)
-document.getElementById("voluntario-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // impede envio padrão do formulário
-
-  emailjs.sendForm("service_f3w95js", "template_xv8exuz", this)
-    .then(function() {
-      alert("Mensagem enviada com sucesso! Obrigado pelo contato.");
-      document.getElementById("voluntario-form").reset();
-    }, function(error) {
-      alert("Ocorreu um erro: " + JSON.stringify(error));
-    });
-});
-
 
 document.getElementById("voluntario-form").addEventListener("submit", function(event) {
   event.preventDefault(); // impede envio padrão do formulário
