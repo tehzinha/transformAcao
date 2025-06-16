751670aed1223d9211ae80ee45f530549f77c1b4
tehzinha <estervrsampaio47@gmail.com>
2025-06-16 20:07:06 -0300

    adicionando lupa no organograma

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ function verOrganograma() {
 function fecharOrganograma() {
   document.getElementById("modalOrganograma").style.display = "none";
 }
+// Lupa
+const imagem = document.getElementById("imagemOrganograma");
+const lupa = document.getElementById("lupa");
+const zoomImagem = document.getElementById("zoomImagem");
+const zoom = 3; // valor do zoom (pode aumentar se quiser)
 
+imagem.addEventListener("mousemove", function(e) {
+  lupa.style.display = "block";
+  const rect = imagem.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  lupa.style.left = (e.clientX - lupa.offsetWidth / 2) + "px";
+  lupa.style.top = (e.clientY - lupa.offsetHeight / 2) + "px";
+
+  zoomImagem.style.width = imagem.width * zoom + "px";
+  zoomImagem.style.height = imagem.height * zoom + "px";
+  zoomImagem.style.left = -x * zoom + lupa.offsetWidth / 2 + "px";
+  zoomImagem.style.top = -y * zoom + lupa.offsetHeight / 2 + "px";
+});
+
+imagem.addEventListener("mouseleave", function() {
+  lupa.style.display = "none";
+});
 
 document.getElementById("voluntario-form").addEventListener("submit", function(event) {
   event.preventDefault(); // impede envio padrão do formulário
